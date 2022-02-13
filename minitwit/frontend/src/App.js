0be547577cd19abaf2bc3c08ddf070af3c87ca4a
Eythor Mikael <eythor108@gmail.com>
2022-02-13 21:21:34 +0000

    implement useNavigate to redirect from login and signup

--- minitwit/frontend/src/App.js
+++ minitwit/frontend/src/App.js
@@ -14,7 +14,6 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-//        <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/timeline' component={Timeline} />
