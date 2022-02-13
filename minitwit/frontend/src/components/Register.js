419bdb38bcdc04155472ac962d5f0e3b99731ea1
Eythor Mikael <eythor108@gmail.com>
2022-02-13 21:21:34 +0000

    implement useNavigate to redirect from login and signup

--- minitwit/frontend/src/components/Register.js
+++ minitwit/frontend/src/components/Register.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-<<<<<<< HEAD:minitwit/frontend/src/components/Register.js
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
