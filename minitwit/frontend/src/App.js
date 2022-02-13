9eeff5fab26d20b1122c6c28d17608eeb0748db0
Eythor Mikael <eythor108@gmail.com>
2022-02-13 19:46:46 +0000

    Refactor frontend

--- minitwit/frontend/src/App.js
+++ minitwit/frontend/src/App.js
@@ -14,6 +14,7 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
+//        <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/timeline' component={Timeline} />
