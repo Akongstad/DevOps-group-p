bb33054997707a42af6e75aeaaa4518732473393
mikaeleythor <70117434+mikaeleythor@users.noreply.github.com>
2022-02-16 21:54:00 +0000

    23 seperate legacy (#27)
    * Refactor frontend
    
    * implement useNavigate to redirect from login and signup
    
    * fix syntax
    
    * close #23, create legacy dir in root, move everything except MinitwitReact/ to legacy/, rename MinitwitReact to minitwit
    
    * Refactor frontend
    
    * implement useNavigate to redirect from login and signup
    
    * fix syntax
    
    * close #23, create legacy dir in root, move everything except MinitwitReact/ to legacy/, rename MinitwitReact to minitwit

--- minitwit/Minitwit.Tests/Minitwit_tests.cs
+++ minitwit/Minitwit.Tests/Minitwit_tests.cs
@@ -1,8 +1,30 @@
+<<<<<<< HEAD
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using MinitwitReact;
+using Xunit;
+
+=======
+>>>>>>> main
 namespace Minitwit.Tests;
 
 public class MinitwitTests : IDisposable
 {
     private readonly IMinitwit _minitwit;
+<<<<<<< HEAD
+
+    public MinitwitTests()
+    {
+        var tempfile = Path.GetTempFileName();
+        var connection = new SqliteConnection($"Data source={tempfile}");
+        connection.Open();
+        _minitwit = new MinitwitReact.Minitwit(connection);
+        _minitwit.InitDb();
+    }
+
+=======
     //EF core
     private readonly IMinitwitContext _context;
 
@@ -54,11 +76,34 @@ public class MinitwitTests : IDisposable
         _minitwit = new MinitwitReact.Minitwit(context, connection);
         //_minitwit.InitDb();
     }
+>>>>>>> main
     [Fact]
     public void Test_if_tempfile_with_schema_created()
     {
         var actual = _minitwit.GetUsers();
         Assert.NotNull(_minitwit);
+<<<<<<< HEAD
+        Assert.NotNull(actual);
+        Assert.NotEqual(0,_minitwit.GetSchema().Rows.Count);
+    }
+
+    [Fact]
+    public void GetUsername_returns_UserName()
+    {
+        var actual = _minitwit.GetUserId("Roger Histand");
+        var actual1 = _minitwit.GetUserId("Geoffrey Stieff");
+        var actual2 = _minitwit.GetUserId("Wendell Ballan");
+        var actual3 = _minitwit.GetUserId("Nathan Sirmon");
+        Assert.Equal(1, actual);
+        Assert.Equal(2, actual1);
+        Assert.Equal(3, actual2);
+        Assert.Equal(4, actual3);
+    }
+    // TEST FOR ADD MESSAGES
+
+    public void Dispose()
+    {
+=======
         Assert.NotNull(actual); 
     }
     [Fact]
@@ -122,6 +167,7 @@ public class MinitwitTests : IDisposable
     public void Dispose()
     {
         Dispose(disposing:true);
+>>>>>>> main
         GC.SuppressFinalize(this);
     }
 }
