9c84fe1e423faae1df582ecdf6b88080a1226908
Akongstad <kongstad@outlook.com>
2022-02-17 19:29:33 +0100

    - add global usings - Setup IMinitwit.cs for Ef core refactor close #40

---
+++ minitwit/MinitwitReact/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System.Data;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.VisualBasic;
+global using MinitwitReact.Core;
+global using MinitwitReact.Entities;
+global using Microsoft.Data.Sqlite;
