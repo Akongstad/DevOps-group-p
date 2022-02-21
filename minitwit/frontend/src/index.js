689e55028cec6ee7e4b795b7c9f9da1a1181ea3f
Eythor Mikael <eythor108@gmail.com>
2022-02-21 20:09:13 +0100

    Add bootstrap and begin implementation of Timeline component

--- minitwit/frontend/src/index.js
+++ minitwit/frontend/src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-
+//import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 ReactDOM.render(
