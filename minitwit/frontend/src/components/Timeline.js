0090810fde2c0c077b2b620559a124db9edc22c4
Eythor Mikael <eythor108@gmail.com>
2022-02-21 20:09:13 +0100

    Add bootstrap and begin implementation of Timeline component

---
+++ minitwit/frontend/src/components/Timeline.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Timeline() {
+    const bootstrap = 'boostarp'; 
+
+}
