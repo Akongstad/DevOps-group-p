e8c9f65fe92f6b25a798d5a76d605a44e94ba296
Eythor Mikael <eythor108@gmail.com>
2022-02-23 17:18:55 +0100

    Add comments to helper script

--- purge_helper.sh
+++ purge_helper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-KEYWORD=$1
-DIRECTORY=$2
-ADD_GREP_PARAM=$3 # Option for additional grep flags
+KEYWORD=$1          # Keyword to search for
+DIRECTORY=$2        # Directory to search in
+ADD_GREP_PARAM=$3   # Option for additional grep flags
 
 if [[ $ADD_GREP_PARAM ]]; then
     STATEMENT="with grep param $ADD_GREP_PARAM"
@@ -10,7 +10,18 @@ else
     STATEMENT=''
 fi
 
-echo;echo;echo
+echo;echo;echo      # Newlines
 echo "Printing all instances of $KEYWORD in $DIRECTORY $STATEMENT"
 echo
-grep -irn $ADD_GREP_PARAM "$DIRECTORY" -e "$KEYWORD" 2>/dev/null | awk '$0 !~ /\/bin\//' | awk '$0 !~ /\/obj\//' | awk '{print $1}'
+
+# Search for all instances of pattern in directory
+grep -irn $ADD_GREP_PARAM "$DIRECTORY" -e "$KEYWORD" 2>/dev/null |
+    
+    # Filter out binary files
+    awk '$0 !~ /\/bin\//' |
+
+    # Filter out this dotnet dir
+    awk '$0 !~ /\/obj\//' |
+
+    # Print the first column (filename + lineno)
+    awk '{print $1}'
