a6a541ca90db46c01a8c5b901eb9532073cdb96d
clyt <clyt@itu.dk>
2022-03-21 23:07:25 +0100

    Refactored Authentication - Added Authenticator and interface. - Added AuthenticatorController.

--- minitwit/backend/MinitwitReact/Core/IRepositories/IUserRepository.cs
+++ minitwit/backend/MinitwitReact/Core/IRepositories/IUserRepository.cs
@@ -2,14 +2,12 @@ namespace MinitwitReact.Core.IRepositories;
 
 public interface IUserRepository
 {
-    Task<long> Login(string username, string pw);
-    Task<long> Register(string username, string email, string pw);
     Task<IEnumerable<MessageDto>> GetPublicTimeline();
     Task<IEnumerable<MessageDto>> GetTimeline(long sessionId, string username);
     Task<IEnumerable<MessageDto>> GetOwnTimeline(long sessionId);
     Task<IEnumerable<UserDto>> GetAllUsers();
     Task<UserDto?> GetUserById(long userId);
-    Task<long> GetUserId(string username);
+    Task<long> GetUserIdFromName(string username);
     Task<UserDetailsDto?> GetUserDetailsById(long userid);
     Task<UserDetailsDto?> GetUserByName(string name);
     Task<Status> PostMessage(long userid, string message);
