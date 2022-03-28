c9ba377d88cfd9a8cd9da45d0e62dbc06c2095d7
Andreas Kongstad <90042173+Akongstad@users.noreply.github.com>
2022-03-28 22:34:30 +0200

    Update run_script.sh

--- minitwit/run_script.sh
+++ minitwit/run_script.sh
@@ -6,12 +6,15 @@ source logging/setup_elk.sh
 #sudo htpasswd -c .htpasswd devops22
 
 git pull 
+
 echo "Taking down frontend" 
 docker stop react_frontend &&
 docker rm react_frontend &&
+
 echo "Taking down backend" &&
 docker stop dotnet_backend &&
 docker rm dotnet_backend &&
+
 echo "Pulling from hub" &&
 docker-compose -f compose.prod.yaml pull &&
 echo "Building containers" &&
