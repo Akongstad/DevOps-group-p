e9a3dd9f26c988f20d658822c4cb96563202defa
mikaeleythor <70117434+mikaeleythor@users.noreply.github.com>
2022-03-29 15:12:13 +0200

    Update run script (#197)
    * remove fe be containers, restart other containers (except grafana)
    
    * update run script to handle new services

--- minitwit/run_script.sh
+++ minitwit/run_script.sh
@@ -3,12 +3,19 @@
 sudo chmod +x logging/setup_elk.sh
 source logging/setup_elk.sh
 
+cd .. 
+
 git pull 
 
 for service in {frontend,backend}; do
     docker-compose -f compose.prod.yaml rm -sf $service && docker-compose -f compose.prod.yaml pull $service && docker-compose -f compose.prod.yaml up -d --no-deps $service;
 done
 
+RUNNING_CONTAINERS="$( docker ps | awk '{print $(NF)}')"
 for service in {db,prometheus,letsencrypt,elasticsearch,kibana,filebeat}; do
-    docker-compose -f compose.prod.yaml restart $service;
+    if [[ "$service" = *"$RUNNING_CONTAINERS"* ]]; then
+        docker-compose -f compose.prod.yaml restart $service;
+    else
+        docker-compose -f compose.prod.yaml up -d --no-deps $service;
+    fi
 done
