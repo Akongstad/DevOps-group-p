e333750140c6d7b424d76012e4e03dcb33b4898d
Eythor Mikael <eythor108@gmail.com>
2022-03-29 15:08:21 +0200

    update run script to handle new services

--- minitwit/run_script.sh
+++ minitwit/run_script.sh
@@ -9,6 +9,11 @@ for service in {frontend,backend}; do
     docker-compose -f compose.prod.yaml rm -sf $service && docker-compose -f compose.prod.yaml pull $service && docker-compose -f compose.prod.yaml up -d --no-deps $service;
 done
 
+RUNNING_CONTAINERS="$( docker ps | awk '{print $(NF)}')"
 for service in {db,prometheus,letsencrypt,elasticsearch,kibana,filebeat}; do
-    docker-compose -f compose.prod.yaml restart $service;
+    if [[ "$service" = *"$RUNNING_CONTAINERS"* ]]; then
+        docker-compose -f compose.prod.yaml restart $service;
+    else
+        docker-compose -f compose.prod.yaml up -d --no-deps $service;
+    fi
 done
