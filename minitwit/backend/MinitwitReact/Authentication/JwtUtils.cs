aba5ca8957b0cc3a320dce67365c703a8391cd0d
Andreas Kongstad <90042173+Akongstad@users.noreply.github.com>
2022-06-01 09:34:55 +0200

    Hotfixnew (#408)
    * Hotfix
    
    * hotfix

--- minitwit/backend/MinitwitReact/Authentication/JwtUtils.cs
+++ minitwit/backend/MinitwitReact/Authentication/JwtUtils.cs
@@ -19,7 +19,8 @@ public class JwtUtils : IJwtUtils
             new(ClaimTypes.Name, user.Username), new(ClaimTypes.NameIdentifier, user.UserId.ToString())
         };
         // generate token that is valid for 1days
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.Secret.ToString()));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.Secret ?? "backup"));
+
 
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
         var token = new JwtSecurityToken(
